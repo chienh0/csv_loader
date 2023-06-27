55aec5d487db7bc4e2a732e8de10210f9601a97d
chienqho <chienqho@gmail.com>
2023-06-27 13:53:40 -0700

    removed test data

--- load_csv_helper.py
+++ load_csv_helper.py
@@ -30,9 +30,9 @@ def create_table(cur, table_name):
     cur.execute(create_table_sql)
 
 
-def insert_data(cur, table_name, data):
-    insert_data_sql = """
+def insert_data(cur, table_name, data_df):
+    insert_sql = """
     INSERT INTO {table_name} (column1, column2, column3) VALUES (%s, %s, %s)
     """.format(table_name=table_name)
-    for row in data:
-        cur.execute(insert_data_sql, (str(row[0]), str(row[1]), str(row[2])))
+    for row in data_df:
+        cur.execute(insert_sql, (str(row[0]), str(row[1]), str(row[2])))
